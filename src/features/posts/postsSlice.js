7a7b8cab0408711a50bb3591a0de7e59e1d19272
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-04 15:33:56 +0100

    feature: added method to post news aysnchronously

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -65,6 +65,10 @@ const postsSlice = createSlice({
           state.status = 'failed'
           state.error = action.error.message
       })
+    builder.addCase(addNewPost.fulfilled, (state, action) => {
+        //We can directly add the new post object to our posts array
+        state.posts.push(action.payload)
+    })
   }
 })
 
@@ -82,3 +86,15 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async ()=> {
     const response = await client.get('/fakeApi/posts')
     return response.data
 })
+
+
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+    // The payload creator receives the partial `{title, content, user}` object
+  async initialPost => {
+    // We send the initial data to the fake API server
+    const response = await client.post('/fakeApi/posts', initialPost)
+    // The response includes the complete post object, including unique ID
+    return response.data
+  }
+)
